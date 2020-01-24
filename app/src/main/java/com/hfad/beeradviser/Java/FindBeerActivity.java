5fdf574737c889e567a46821545255ba214bdedd
sado98 <sado9898kz@gmail.com>
2020-01-24 15:33:44 +0600

    Create project "Beer Adviser".

---
+++ app/src/main/java/com/hfad/beeradviser/Java/FindBeerActivity.java
@@ -0,0 +1,16 @@
+package com.hfad.beeradviser.Java;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.hfad.beeradviser.R;
+
+public class FindBeerActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_find_beer);
+    }
+}
---
+++ app/src/main/java/com/hfad/beeradviser/Kotlin/FindBeerActivity.kt
@@ -0,0 +1,12 @@
+package com.hfad.beeradviser.Kotlin
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class FindBeerActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_find_beer)
+    }
+}
