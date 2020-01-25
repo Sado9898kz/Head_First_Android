66d16e800b7a273170d90f4d3f7c65cfc8e5a8d3
sado98 <sado9898kz@gmail.com>
2020-01-25 13:01:36 +0600

    Configuration second activity and his layout.

---
+++ app/src/main/java/com/hfad/mymessenger/Java/ReceiveMessageActivity.java
@@ -0,0 +1,16 @@
+package com.hfad.mymessenger.Java;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.hfad.mymessenger.R;
+
+public class ReceiveMessageActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_receive_message);
+    }
+}
