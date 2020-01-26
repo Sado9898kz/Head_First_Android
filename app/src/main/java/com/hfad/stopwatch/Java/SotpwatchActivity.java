252b5721dc484a93aab1e761a7f55d19af4a9751
sado98 <sado9898kz@gmail.com>
2020-01-26 15:26:02 +0600

    Create project "Stopwatch". And prepared layout for project.

---
+++ app/src/main/java/com/hfad/stopwatch/Java/SotpwatchActivity.java
@@ -0,0 +1,16 @@
+package com.hfad.stopwatch.Java;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.hfad.stopwatch.R;
+
+public class SotpwatchActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sotpwatch);
+    }
+}
---
+++ app/src/main/java/com/hfad/stopwatch/Kotlin/StopwatchActivity.kt
@@ -0,0 +1,13 @@
+package com.hfad.stopwatch.Kotlin
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.hfad.stopwatch.R
+
+class StopwatchActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_sotpwatch)
+    }
+}
