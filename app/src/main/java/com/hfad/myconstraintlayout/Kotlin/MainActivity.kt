31082caaa457f101d1572b42cd44d7170512d6df
sado98 <sado9898kz@gmail.com>
2020-01-28 16:21:19 +0600

    How work Constraint Layout.

---
+++ app/src/main/java/com/hfad/myconstraintlayout/Kotlin/MainActivity.kt
@@ -0,0 +1,13 @@
+package com.hfad.myconstraintlayout.Kotlin
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.hfad.myconstraintlayout.R
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
