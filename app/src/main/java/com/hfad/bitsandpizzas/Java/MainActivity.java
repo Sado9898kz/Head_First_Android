49d27fc3b016aa4427e5d79e38777d7e3bd2eef2
sado98 <sado9898kz@gmail.com>
2020-01-31 15:58:07 +0600

    Theme, colors, and how add attribute in style.

---
+++ app/src/main/java/com/hfad/bitsandpizzas/Java/MainActivity.java
@@ -0,0 +1,16 @@
+package com.hfad.bitsandpizzas.Java;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.hfad.bitsandpizzas.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
