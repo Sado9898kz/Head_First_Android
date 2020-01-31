ae00e27df4785b94b28f51ceede806dcc01d0772
sado98 <sado9898kz@gmail.com>
2020-01-31 16:47:47 +0600

    Layout inside in layout. And how create castum ToolBar.

--- app/src/main/java/com/hfad/bitsandpizzas/Java/MainActivity.java
+++ app/src/main/java/com/hfad/bitsandpizzas/Java/MainActivity.java
@@ -1,6 +1,7 @@
 package com.hfad.bitsandpizzas.Java;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
 import android.os.Bundle;
 
@@ -12,5 +13,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
     }
 }
--- app/src/main/java/com/hfad/bitsandpizzas/Kotlin/MainActivity.kt
+++ app/src/main/java/com/hfad/bitsandpizzas/Kotlin/MainActivity.kt
@@ -2,12 +2,16 @@ package com.hfad.bitsandpizzas.Kotlin
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.widget.Toolbar
 import com.hfad.bitsandpizzas.R
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val toolbar = toolbar as Toolbar
+        setSupportActionBar(toolbar)
     }
 }
