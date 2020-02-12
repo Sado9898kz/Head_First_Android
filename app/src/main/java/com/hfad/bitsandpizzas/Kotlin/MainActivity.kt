d52bf3338261d3bde9e1db46db503427325e35a8
sado98 <sado9898kz@gmail.com>
2020-02-12 15:57:18 +0600

    Create ViewPager and swipe fragment.

--- app/src/main/java/com/hfad/bitsandpizzas/Kotlin/MainActivity.kt
+++ app/src/main/java/com/hfad/bitsandpizzas/Kotlin/MainActivity.kt
@@ -8,8 +8,12 @@ import android.view.MenuItem
 import androidx.appcompat.widget.ShareActionProvider
 import androidx.appcompat.widget.Toolbar
 import androidx.core.view.MenuItemCompat
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentManager
+import androidx.viewpager.widget.ViewPager
 import com.hfad.bitsandpizzas.R
 import kotlinx.android.synthetic.main.activity_main.*
+import androidx.fragment.app.FragmentPagerAdapter as FragmentPagerAdapter1
 
 class MainActivity : AppCompatActivity() {
 
@@ -20,6 +24,11 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
         val toolbar = toolbar as Toolbar
         setSupportActionBar(toolbar)
+
+        //Связывание SectionsPagerAdapter c ViewPager
+        val pagerAdapter = SectionsPagerAdapter(supportFragmentManager)
+        val pager = findViewById<ViewPager>(R.id.pager)
+        pager.adapter = pagerAdapter
     }
 
     override fun onCreateOptionsMenu(menu: Menu?): Boolean {
@@ -31,13 +40,6 @@ class MainActivity : AppCompatActivity() {
         return super.onCreateOptionsMenu(menu)
     }
 
-    private fun setShareActionProvider(text: String) {
-        val intent = Intent(Intent.ACTION_SEND)
-        intent.type = "text/plain"
-        intent.putExtra(Intent.EXTRA_TEXT, text)
-        sharedActionProvider.setShareIntent(intent)
-    }
-
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
         return when (item.itemId) {
             R.id.action_create_order -> {
@@ -46,6 +48,27 @@ class MainActivity : AppCompatActivity() {
                 true
             }
             else -> super.onOptionsItemSelected(item)
+        }
+    }
+
+    private fun setShareActionProvider(text: String) {
+        val intent = Intent(Intent.ACTION_SEND)
+        intent.type = "text/plain"
+        intent.putExtra(Intent.EXTRA_TEXT, text)
+        sharedActionProvider.setShareIntent(intent)
+    }
+
+    class SectionsPagerAdapter(fm: FragmentManager) : FragmentPagerAdapter1(fm) {
+        override fun getCount(): Int {
+            return 4
+        }
+
+        override fun getItem(position: Int): Fragment = when (position) {
+            0 -> TopFragment()
+            1 -> PizzaFragment()
+            2 -> PastaFragment()
+            3 -> StoresFragment()
+            else -> Fragment()
         }
     }
 }
