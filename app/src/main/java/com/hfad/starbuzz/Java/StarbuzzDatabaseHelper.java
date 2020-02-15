60dea8faf52a6b9e6b34d654bb4a909f45a87ef1
sado98 <sado9898kz@gmail.com>
2020-02-15 18:06:17 +0600

    Add BD info.

--- app/src/main/java/com/hfad/starbuzz/Java/StarbuzzDatabaseHelper.java
+++ app/src/main/java/com/hfad/starbuzz/Java/StarbuzzDatabaseHelper.java
@@ -1,13 +1,16 @@
 package com.hfad.starbuzz.Java;
 
+import android.content.ContentValues;
 import android.content.Context;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
 
+import com.hfad.starbuzz.R;
+
 public class StarbuzzDatabaseHelper extends SQLiteOpenHelper {
 
-    private static final String DB_NAME = "starbuzz";
-    private static final int DB_VERSION = 1;
+    private static final String DB_NAME = "starbuzz"; //Имя базы данных
+    private static final int DB_VERSION = 1; //Версия базы данных
 
     StarbuzzDatabaseHelper(Context context) {
         super(context, DB_NAME, null, DB_VERSION);
@@ -15,11 +18,25 @@ public class StarbuzzDatabaseHelper extends SQLiteOpenHelper {
 
     @Override
     public void onCreate(SQLiteDatabase db) {
-
+        db.execSQL("CREATE TABLE DRINK (_id INTEGER PRIMARY KEY AUTOINCREMENT, "
+                + "NAME TEXT, "
+                + "DESCRIPTION TEXT, "
+                + "IMAGE_RESOURCE_ID INTEGER);");
+        insertDrink(db, "Молоко", "Пара порций эспрессо с парным молоком", R.drawable.latte);
+        insertDrink(db, "Капучино", "Эспрессо, горячее молоко и молочная пена на пару", R.drawable.cappuccino);
+        insertDrink(db, "Фильтр", "Бобы высшего качества, обжаренные и сваренные в свежем виде", R.drawable.filter);
     }
 
     @Override
     public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
 
+    }
+
+    private static void insertDrink(SQLiteDatabase db, String name, String description, int resourceId) {
+        ContentValues drinkValues = new ContentValues();
+        drinkValues.put("NAME", name);
+        drinkValues.put("DESCRIPTION", description);
+        drinkValues.put("IMAGE_RESOURCE_ID", resourceId);
+        db.insert("DRINK", null, drinkValues);
     }
 }
--- app/src/main/java/com/hfad/starbuzz/Kotlin/StarbuzzDatabaseHelper.kt
+++ app/src/main/java/com/hfad/starbuzz/Kotlin/StarbuzzDatabaseHelper.kt
@@ -1,15 +1,43 @@
 package com.hfad.starbuzz.Kotlin
 
+import android.content.ContentValues
 import android.content.Context
 import android.database.sqlite.SQLiteDatabase
 import android.database.sqlite.SQLiteOpenHelper
+import com.hfad.starbuzz.R
 
 class StarbuzzDatabaseHelper(context: Context) : SQLiteOpenHelper(context, "starbuzz", null, 1) {
     override fun onCreate(db: SQLiteDatabase?) {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+        db!!.execSQL(
+            "CREATE TABLE DRINK (_id INTEGER PRIMARY KEY AUTOINCREMENT, "
+                    + "NAME TEXT, "
+                    + "DESCRIPTION TEXT, "
+                    + "IMAGE_RESOURCE_ID INTEGER);"
+        )
+        insertDrink(db, "Молоко", "Пара порций эспрессо с парным молоком", R.drawable.latte)
+        insertDrink(
+            db,
+            "Капучино",
+            "Эспрессо, горячее молоко и молочная пена на пару",
+            R.drawable.cappuccino
+        )
+        insertDrink(
+            db,
+            "Фильтр",
+            "Бобы высшего качества, обжаренные и сваренные в свежем виде",
+            R.drawable.filter
+        )
     }
 
     override fun onUpgrade(db: SQLiteDatabase?, oldVersion: Int, newVersion: Int) {
         TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    fun insertDrink(db: SQLiteDatabase, name: String, description: String, resourceId: Int) {
+        val drinkValues = ContentValues()
+        drinkValues.put("NAME", name)
+        drinkValues.put("DESCRIPTION", description)
+        drinkValues.put("IMAGE_RESOURCE_ID", resourceId)
+        db.insert("DRINK", null, drinkValues)
     }
 }
