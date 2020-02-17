05af6f691fcc613baf19c2ec93b81f55d3b47af0
sado98 <sado9898kz@gmail.com>
2020-02-17 13:28:18 +0600

    Create cursor for db.

--- app/src/main/java/com/hfad/starbuzz/Java/DrinkActivity.java
+++ app/src/main/java/com/hfad/starbuzz/Java/DrinkActivity.java
@@ -1,10 +1,15 @@
 package com.hfad.starbuzz.Java;
 
-import androidx.appcompat.app.AppCompatActivity;
-
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteException;
+import android.database.sqlite.SQLiteOpenHelper;
 import android.os.Bundle;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 import com.hfad.starbuzz.R;
 
@@ -19,7 +24,20 @@ public class DrinkActivity extends AppCompatActivity {
 
         //Получить напитки из данных интента
         int drinkId = (Integer) getIntent().getExtras().get(EXTRA_DRINKID);
-        Drink drink = Drink.drinks[drinkId];
+
+        //Создать курсор
+        SQLiteOpenHelper starbuzzDatabaseHelper = new StarbuzzDatabaseHelper(this);
+        try {
+            SQLiteDatabase db = starbuzzDatabaseHelper.getReadableDatabase();
+            Cursor cursor = db.query("DRINK",
+                    new String[]{"NAME", "DESCRIPTION", "IMAGE_RESOURCE_ID"},
+                    "_id = ?",
+                    new String[]{Integer.toString(drinkId)},
+                    null, null, null);
+        } catch (SQLiteException e) {
+            Toast toast = Toast.makeText(this, "База данных недоступна", Toast.LENGTH_SHORT);
+            toast.show();
+        }
 
         //Заполнение названия напитка
         TextView name = (TextView) findViewById(R.id.name);
--- app/src/main/java/com/hfad/starbuzz/Kotlin/DrinkActivity.kt
+++ app/src/main/java/com/hfad/starbuzz/Kotlin/DrinkActivity.kt
@@ -1,9 +1,12 @@
 package com.hfad.starbuzz.Kotlin
 
-import androidx.appcompat.app.AppCompatActivity
+import android.database.sqlite.SQLiteException
 import android.os.Bundle
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
 import com.hfad.starbuzz.R
 import kotlinx.android.synthetic.main.activity_drink.*
+
 
 class DrinkActivity : AppCompatActivity() {
 
@@ -15,7 +18,20 @@ class DrinkActivity : AppCompatActivity() {
 
         //Получить напитки из данных интента
         val drinkId = intent.extras!![EXTRA_DRINKID] as Int
-        val drink = Drink.drinks[drinkId]
+
+        //Создать курсор
+        val starbuzzDatabaseHelper = StarbuzzDatabaseHelper(this)
+        try {
+            val db = starbuzzDatabaseHelper.readableDatabase
+            val cursor = db.query(
+                "DRINK",
+                arrayOf("NAME", "DESCRIPTION", "IMAGE_RESOURCE_ID"),
+                "_id = ?", arrayOf(drinkId.toString()),
+                null, null, null
+            )
+        } catch (e: SQLiteException) {
+            Toast.makeText(this, "База данных недоступна", Toast.LENGTH_SHORT).show()
+        }
 
         //Заполнение названия напитка
         val name = name
