fcc4d2c80f0d2d1f8acd00a213d53454f5d06822
sado98 <sado9898kz@gmail.com>
2020-02-17 14:33:12 +0600

    Create adapter for cursor.

--- app/src/main/java/com/hfad/starbuzz/Java/DrinkCategoryActivity.java
+++ app/src/main/java/com/hfad/starbuzz/Java/DrinkCategoryActivity.java
@@ -1,28 +1,53 @@
 package com.hfad.starbuzz.Java;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Intent;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteException;
+import android.database.sqlite.SQLiteOpenHelper;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.widget.SimpleCursorAdapter;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 import com.hfad.starbuzz.R;
 
 public class DrinkCategoryActivity extends AppCompatActivity {
 
+    private SQLiteDatabase db;
+    private Cursor cursor;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_drink_category);
+        ListView listDrinks = (ListView) findViewById(R.id.list_drink);
+
+        SQLiteOpenHelper starbuzzDatabaseHelper = new StarbuzzDatabaseHelper(this);
+        try {
+            db = starbuzzDatabaseHelper.getReadableDatabase();
+            cursor = db.query("DRINK", new String[]{"_id", "NAME"},
+                    null, null, null, null, null);
+            SimpleCursorAdapter listAdapter = new SimpleCursorAdapter(this,
+                    android.R.layout.simple_list_item_1,
+                    cursor,
+                    new String[]{"NAME"},
+                    new int[]{android.R.id.text1},
+                    0);
+            listDrinks.setAdapter(listAdapter);
+        } catch (SQLiteException e) {
+            Toast toast = Toast.makeText(this, "База данных недоступна", Toast.LENGTH_SHORT);
+            toast.show();
+        }
         ArrayAdapter<Drink> listAdapter = new ArrayAdapter<>(
                 this,
                 android.R.layout.simple_list_item_1,
                 Drink.drinks);
-        ListView listDrinks = (ListView) findViewById(R.id.list_drink);
-        listDrinks.setAdapter(listAdapter);
 
         //Создание слушателя
         AdapterView.OnItemClickListener itemClickListener =
@@ -36,7 +61,14 @@ public class DrinkCategoryActivity extends AppCompatActivity {
                     }
                 };
 
-        //Назначения слушаетля для спискового представления
+        //Слушаетля связывается со спискового представления
         listDrinks.setOnItemClickListener(itemClickListener);
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        cursor.close();
+        db.close();
     }
 }
--- app/src/main/java/com/hfad/starbuzz/Kotlin/DrinkCategoryActivity.kt
+++ app/src/main/java/com/hfad/starbuzz/Kotlin/DrinkCategoryActivity.kt
@@ -1,20 +1,45 @@
 package com.hfad.starbuzz.Kotlin
 
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
+import android.database.Cursor
+import android.database.sqlite.SQLiteDatabase
+import android.database.sqlite.SQLiteException
 import android.os.Bundle
-import android.widget.ArrayAdapter
+import android.widget.SimpleCursorAdapter
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
 import com.hfad.starbuzz.R
 import kotlinx.android.synthetic.main.activity_drink_category.*
 
 class DrinkCategoryActivity : AppCompatActivity() {
 
+    private lateinit var cursor: Cursor
+    private lateinit var db: SQLiteDatabase
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_drink_category)
-        val listAdapter = ArrayAdapter(this, android.R.layout.simple_list_item_1, Drink.drinks)
         val listDrinks = list_drink
-        listDrinks.adapter = listAdapter
+
+        val starbuzzDatabaseHelper = StarbuzzDatabaseHelper(this)
+        try {
+            db = starbuzzDatabaseHelper.readableDatabase
+            cursor = db.query(
+                "DRINK", arrayOf("_id", "NAME"),
+                null, null, null, null, null
+            )
+            val listAdapter = SimpleCursorAdapter(
+                this,
+                android.R.layout.simple_list_item_1,
+                cursor,
+                arrayOf("NAME"),
+                intArrayOf(android.R.id.text1),
+                0
+            )
+            listDrinks.adapter = listAdapter
+        } catch (e: SQLiteException) {
+            Toast.makeText(this, "База данных недоступна", Toast.LENGTH_SHORT).show()
+        }
 
         //Создание слушателя
         listDrinks.setOnItemClickListener { _, _, _, id ->
@@ -23,5 +48,11 @@ class DrinkCategoryActivity : AppCompatActivity() {
             intent.putExtra(DrinkActivity().EXTRA_DRINKID, id.toInt())
             startActivity(intent)
         }
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        cursor.close()
+        db.close()
     }
 }
